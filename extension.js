427cd4003a43146fd38f38f9ef05fc62d9d5fc1c
hjuangsong <huangsong>
2024-07-08 19:30:39 +0800

    代码提交

--- extension.js
+++ extension.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: hjuangsong huangsong
+ * @Date: 2024-06-26 14:17:44
+ * @LastEditors: hjuangsong huangsong
+ * @LastEditTime: 2024-07-08 19:27:11
+ * @FilePath: \create-react-component-song\extension.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 'use strict';
 const vscode = require('vscode');
 const paramCase = require('change-case').paramCase;
@@ -23,8 +31,15 @@ function activate(context) {
 
         let componentName = paramCase(val);
         // if 是初始化页面写入的路径 else 初始化页面整体模板
-        let componentDir = type === 'initReactComponent' ? generators.createComponentDir(uri, componentName, true)
-          : generators.createComponentDir(uri, componentName);
+        let componentDir = null;
+        if (type === 'initReactComponent') {
+          componentDir = generators.createComponentDir(uri, componentName, true);
+        }
+        else if (type === 'createReactClassComponent') {
+          componentDir = generators.createComponentDir(uri, componentName);
+        } else if (type === 'createReactActivityClassComponent') {
+          componentDir = generators.createComponentDir(uri, componentName);
+        }
         // if 是初始化页面写入的路径 else 初始化页面整体模板
         if (type === 'initReactComponent') {
           return Promise.all([
@@ -33,14 +48,37 @@ function activate(context) {
             generators.createCSS(componentDir, componentName, type),
           ]);
         }
-        return Promise.all([
-          generators.createComponent(componentDir, componentName, type),
-          generators.createModalComponent(componentDir, componentName, type),
-          generators.createTestFile(componentDir, componentName),
-          generators.createCSS(componentDir, componentName),
-          generators.createModelFile(componentDir, componentName),
-          generators.createServiceFile(componentDir, componentName)
-        ]);
+        else if (type === 'createReactClassComponent') {
+          return Promise.all([
+            generators.createComponent(componentDir, componentName, type),
+            generators.createModalComponent(componentDir, componentName, type),
+            generators.createTestFile(componentDir, componentName),
+            generators.createCSS(componentDir, componentName),
+            generators.createAddInfoFile(componentDir, componentName),
+            generators.createModelFile(componentDir, componentName),
+            generators.createServiceFile(componentDir, componentName),
+            generators.updateRouterTopFile(componentDir, componentName),
+            generators.updateRouterLastFile(componentDir, componentName),
+            generators.updateIndexJS(componentDir, componentName),
+          ]);
+        } else if (type === 'createReactActivityClassComponent') {
+          return Promise.all([
+            generators.createComponent(componentDir, componentName, type),
+            generators.createModalComponent(componentDir, componentName, type),
+            generators.createTestFile(componentDir, componentName),
+            generators.createCSS(componentDir, componentName),
+            generators.createAddInfoFile(componentDir, componentName,'activity'),
+            generators.createActivityStepOneFile(componentDir, componentName),
+            generators.createActivityStepTwoFile(componentDir, componentName),
+            generators.createActivityProductComponent(componentDir, componentName),
+            generators.createActivityCss(componentDir, componentName),
+            generators.createModelFile(componentDir, componentName),
+            generators.createServiceFile(componentDir, componentName),
+            generators.updateRouterTopFile(componentDir, componentName),
+            generators.updateRouterLastFile(componentDir, componentName),
+            generators.updateIndexJS(componentDir, componentName),
+          ]);
+        }
       })
       .then(
         () => logger('success', 'React component successfully created!'),
@@ -52,6 +90,10 @@ function activate(context) {
     {
       type: 'class',
       commandID: 'extension.createReactClassComponent'
+    },
+    {
+      type: 'createReactActivityClassComponent',
+      commandID: 'extension.createReactActivityClassComponent'
     },
     {
       type: 'initReactComponent',
